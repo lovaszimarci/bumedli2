b126aa2e996d3da383e88a43819608d3ce656430
Lovászi Márton <lovaszimarci@gmail.com>
2021-11-25 21:29:57 +0100

    kommenteket tenni

---
+++ bumedli.py
@@ -0,0 +1,15 @@
+class Bumedli:
+    #zozi_counter_list = []
+    #marci_counter_list = []
+    def __init__(self, counter,):
+        self.counter = counter
+
+    def mango(self, megadando_pont):
+        return megadando_pont + self.counter
+
+zozi = Bumedli()
+marci = Bumedli()
+
+
+print(marci.mango(11))
+print(marci.counter)
