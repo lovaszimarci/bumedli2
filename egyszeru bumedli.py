dd2611f2c3a5a24adbe5cd2828efbbad1ce0a762
Lovászi Márton <lovaszimarci@gmail.com>
2021-11-29 00:47:14 +0100

    'nemaveglegesdesikerul'

--- egyszeru bumedli.py
+++ egyszeru bumedli.py
@@ -1,19 +1,33 @@
-def pontszamiro(pont):
-    with open('marcipont.txt', 'w') as file:
-        file.write(pont)
-        file.close()
-        pass
+with open('marcipont.txt','r') as file:
+    intek = file.read()
 
-def pontszamolvaso():
-    with open('marcipont.txt', 'r') as file:
-        return file.read()
+kivett_szam = [int(i) for i in intek.split() if i.isdigit()]
+sima_szam = kivett_szam[0]
+osszeadott_szam = sima_szam + 3
+vegleges_szam = str(osszeadott_szam)
 
-
-pontszamiro(22)
-print(pontszamolvaso())
-
-# itt az a gond hogy ez csak at irja nem pedig osszeadja oket
-# de a write es a read def alapja mar megvan
+with open('marcipont.txt', 'w') as file:
+    file.write(vegleges_szam)
+    file.close()
 
 
-# read from file --> aztan egy varieblehez hozza adni egy szamot --> osszeadni aztan writeolni a filet miutan string 
+# def pontszam(pont):
+#     with open('marcipont.txt', 'r') as file:
+#         alapszam = file.read()
+#         fil
+#
+#         total = sum(alapszam.split(',') + list((pont)))
+#         print(total)
+#     #     pass
+#     # file2 = int(alapszam) + int(pont)
+#     # with open('marcipont.txt', 'w') as file:
+#     #     file.write(str(file2))
+#     #     return file.read()
+#
+# pontszam(2)
+#
+# # itt az a gond hogy ez csak at irja nem pedig osszeadja oket
+# # de a write es a read def alapja mar megvan
+#
+#
+# # read from file --> aztan egy varieblehez hozza adni egy szamot --> osszeadni aztan writeolni a filet miutan string
