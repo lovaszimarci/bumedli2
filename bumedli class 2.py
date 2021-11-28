4fb2dc29efa989d200dbca3cd843339333160c4c
Lovászi Márton <lovaszimarci@gmail.com>
2021-11-28 12:17:13 +0100

    'reades_function'

--- bumedli class 2.py
+++ bumedli class 2.py
@@ -1,18 +1,29 @@
 class Marci():
-   pontszam = 0
+    def __init__(self, pontok):
+        self.pontok = pontok
+    def pontiro(self, pontszam):
+        with open('marcipont.txt', 'w') as file:
+            file.write(pontszam)
+            file.close()
+            pass
+marci = Marci()
 
-   @classmethod
-   def update(cls, value):
-       cls.pontszam += value
+print(marci.pontok)
 
-   def __init__(self, value):
-       self.value = value
-       self.update(value)
 
-marci = Marci(0)
 
-Marci.update(5)
-Marci.update(6)
-
-print(Marci.pontszam)
-
+#    @classmethod
+#    def update(cls, value):
+#        cls.pontszam += value
+#
+#    def __init__(self, value):
+#        self.value = value
+#        self.update(value)
+#
+# marci = Marci(0)
+#
+# Marci.update(5)
+# Marci.update(6)
+#
+# print(Marci.pontszam)
+#
