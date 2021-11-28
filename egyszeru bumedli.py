a313ea028ca17e3380966a3322f1f5db0ac56793
Lovászi Márton <lovaszimarci@gmail.com>
2021-11-28 13:00:20 +0100

    'kommentalenyeg'

--- egyszeru bumedli.py
+++ egyszeru bumedli.py
@@ -1,6 +1,6 @@
 def pontszamiro(pont):
     with open('marcipont.txt', 'w') as file:
-        file.write(str(pont))
+        file.write(pont)
         file.close()
         pass
 
@@ -9,8 +9,11 @@ def pontszamolvaso():
         return file.read()
 
 
-pontszamiro(66)
+pontszamiro(22)
 print(pontszamolvaso())
 
 # itt az a gond hogy ez csak at irja nem pedig osszeadja oket
 # de a write es a read def alapja mar megvan
+
+
+# read from file --> aztan egy varieblehez hozza adni egy szamot --> osszeadni aztan writeolni a filet miutan string 
