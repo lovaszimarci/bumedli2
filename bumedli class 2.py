c4596b4647b6526de9ba7cf21f6e24867e042af2
Lovászi Márton <lovaszimarci@gmail.com>
2021-11-29 01:19:58 +0100

    'zoziesmarcidefegybefuzbevegleges'

--- bumedli class 2.py
+++ bumedli class 2.py
@@ -1,36 +1,31 @@
 class Marci():
- with open('marcipont.txt', 'w') as file:
-    file.write('0')
-    file.close()
-    pass
+    def __init__(self, név):
+     self.név = név
+
+
+     def marcipontszamado(self,szam):
+        with open('marcipont.txt', 'r') as file:
+            intek = file.read()
+
+        kivett_szam = [int(i) for i in intek.split() if i.isdigit()]
+        sima_szam = kivett_szam[0]
+        osszeadott_szam = sima_szam + szam
+        vegleges_szam = str(osszeadott_szam)
+
+        with open('marcipont.txt', 'w') as file:
+            file.write(vegleges_szam)
+            file.close()
+            pass
+    def pontok_(self):
+        with open('marcipont.txt', 'r') as file:
+            pontjaim = file.read()
+            print(pontjaim)
+            pass
 
 
 
-    # def __init__(self, pontok):
-    #     self.pontok = pontok
-    # def pontiro(self, pontszam):
-    #     with open('marcipont.txt', 'w') as file:
-    #         file.write(pontszam)
-    #         file.close()
-    #         pass
-#marci = Marci()
+
+marci = Marci('marci')
 
 
 
-
-
-#    @classmethod
-#    def update(cls, value):
-#        cls.pontszam += value
-#
-#    def __init__(self, value):
-#        self.value = value
-#        self.update(value)
-#
-# marci = Marci(0)
-#
-# Marci.update(5)
-# Marci.update(6)
-#
-# print(Marci.pontszam)
-#
--- bumedliszamlalo def.py
+++ bumedliszamlalo def.py
@@ -1,6 +1,7 @@
-def pontszam(szam):
+def marcipontszamado(szam):
     with open('marcipont.txt','r') as file:
         intek = file.read()
+        file.close()
 
     kivett_szam = [int(i) for i in intek.split() if i.isdigit()]
     sima_szam = kivett_szam[0]
@@ -12,7 +13,34 @@ def pontszam(szam):
         file.close()
         pass
 
-pontszam(10)
+def zozipontszamado(szam):
+    with open('zozipont.txt','r') as file:
+        intek = file.read()
+
+    kivett_szam = [int(i) for i in intek.split() if i.isdigit()]
+    sima_szam = kivett_szam[0]
+    osszeadott_szam = sima_szam + szam
+    vegleges_szam = str(osszeadott_szam)
+
+    with open('zozipont.txt', 'w') as file:
+        file.write(vegleges_szam)
+        file.close()
+        pass
+
+def marcipontok():
+    with open('marcipont.txt', 'r') as file:
+        pontok = file.read()
+        file.close()
+    print(f"Márton pontjai: {pontok}")
+    pass
+
+def zozipontok():
+    with open('zozipont.txt', 'r') as file:
+        pontok = file.read()
+        file.close()
+    print(f'Zoltán pontjai: {pontok}')
+    pass
+
 
 # lepesek: 1 beolvassa a txt
 # 2 egy for loopal kiszedi egy listaba a szamjegyeket
@@ -20,3 +48,4 @@ pontszam(10)
 # 4 az int variablet es def ben megadott numot ossze adja
 # a vegleges_szam pedig at konvertalja az ossze adott intet strbe mert csak azt tudja writeolni
 # a vegleges szammra kicserelni a txtben az alap szamot es bezarul
+# a vegen a fugvenyek meg igazabol csak megjelenitik az elert pontokat
